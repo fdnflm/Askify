170fc159ff46258f08936d9d4a410f2e6068a2be
fdnflm <berezin.albert@gmail.com>
2020-08-13 23:34:04 +0300

    added moment.js

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
+from flask_moment import Moment
 
 app = Flask(__name__)
 app.config.from_object("config")
 db = SQLAlchemy(app)
 mail = Mail(app)
+moment = Moment(app)
 login_manager = LoginManager(app)
 login_manager.login_view = "login"
